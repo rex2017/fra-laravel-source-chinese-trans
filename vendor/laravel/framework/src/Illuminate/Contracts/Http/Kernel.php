d78469a1c5c8ffce629dca0103149a7b63e01422
wde4lee <hnaaa@foxmail.com>
2024-02-04 11:06:39 +0800

    20240203-增加翻译

---
+++ vendor/laravel/framework/src/Illuminate/Contracts/Http/Kernel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Illuminate\Contracts\Http;
+
+/**
+ * Kernel 内核接口
+ */
+interface Kernel
+{
+    /**
+     * Bootstrap the application for HTTP requests.
+     *
+     * @return void
+     */
+    public function bootstrap();
+
+    /**
+     * Handle an incoming HTTP request.
+     *
+     * @param  \Symfony\Component\HttpFoundation\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle($request);
+
+    /**
+     * Perform any final actions for the request lifecycle.
+     *
+     * @param  \Symfony\Component\HttpFoundation\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return void
+     */
+    public function terminate($request, $response);
+
+    /**
+     * Get the Laravel application instance.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application
+     */
+    public function getApplication();
+}
