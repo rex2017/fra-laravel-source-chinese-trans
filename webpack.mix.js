2f35109b149cc33a570c7e34aa04467499ad536c
wde4lee <adeee@foxmail.com>
2025-05-23 21:31:21 +0800

    20250523-更新

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,6 +9,8 @@ const mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
+ | 混合提供了一个干净、流利的API来定义一些Webpack构建步骤为你的Laravel应用。
+ | 默认情况下，我们编译应用的Sass文件以及捆绑所有JS文件。
  |
  */
 
